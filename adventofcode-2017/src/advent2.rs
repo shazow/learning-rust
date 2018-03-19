ea93a479ba3fc48acf29d2faf16a833816a02d6c
Andrey Petrov <andrey.petrov@shazow.net>
2018-03-19 15:36:06 -0400

    Refactor with load_input helper

--- adventofcode-2017/src/advent2.rs
+++ adventofcode-2017/src/advent2.rs
@@ -1,10 +1,12 @@
-pub fn advent2part1(input:&str) -> u32 {
+pub fn advent2part1(input: &str) -> u32 {
     let mut acc = 0;
     for line in input.lines() {
-        let parsed: Vec<u32> = line.split_whitespace().filter_map(|w| w.parse().ok()).collect();
+        let parsed: Vec<u32> = line.split_whitespace()
+            .filter_map(|w| w.parse().ok())
+            .collect();
         let min = parsed.iter().min().expect("no values");
         let max = parsed.iter().max().expect("no values");
-        acc += max-min;
+        acc += max - min;
     }
     return acc;
 }
@@ -14,12 +16,17 @@ mod tests {
     use super::*;
     use std::fs::File;
     use std::io::prelude::*;
+    use std::error::Error;
+
+    fn load_input(path: &str) -> Result<String, Box<Error>> {
+        let mut buf = String::new();
+        File::open(path)?.read_to_string(&mut buf)?;
+        Ok(buf)
+    }
 
     #[test]
     fn test_advent2() {
-        let mut f = File::open("src/advent2.input").expect("file not found");
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("something went wrong reading the file");
+        let contents = load_input("src/advent2.input").expect("load input failed");
         let res = advent2part1(&contents);
         assert_eq!(30994, res);
     }
